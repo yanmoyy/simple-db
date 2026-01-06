ef4e41c4a1d46a43b4aebcc50195aebf72e7da72
yanmo <67538999+yanmoyy@users.noreply.github.com>
2026-01-06 13:28:25 +0900

    2. Statement

--- include/db.h
+++ include/db.h
@@ -1,19 +1,47 @@
 #ifndef DB_H
 #define DB_H
 
-#include <stdio.h>
+#include "input_buffer.h"
 
-typedef struct InputBuffer {
-    char *buffer;
-    size_t buffer_length;
-    ssize_t input_length;
-} InputBuffer;
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+#define PAGE_SIZE 4096
+#define TABLE_MAX_PAGES 400
+
+typedef enum {
+    NODE_INTERNAL,
+    NODE_LEAF,
+} NodeType;
+
+typedef enum {
+    EXECUTE_SUCCESS,
+    EXECUTE_DUPLICATE_KEY,
+} ExecuteResult;
+
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND,
+} MetaCommandResult;
+
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT,
+} PrepareResult;
+
+typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT,
+} StatementType;
+
+typedef struct {
+    StatementType type;
+} Statement;
 
 /* Public functions */
-InputBuffer *new_input_buffer();
-void close_input_buffer(InputBuffer *input_buffer);
 void print_prompt();
-void read_input(InputBuffer *input_buffer);
 void repl(InputBuffer *input_buffer);
+MetaCommandResult do_meta_command(InputBuffer *input_buffer);
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement);
+void execute_statement(Statement *statement);
 
 #endif
--- src/repl.c
+++ src/repl.c
@@ -9,10 +9,64 @@ void repl(InputBuffer *input_buffer)
 {
     print_prompt();
     read_input(input_buffer);
+
     if (strcmp(input_buffer->buffer, ".exit") == 0) {
-        close_input_buffer(input_buffer);
+        if (input_buffer->buffer[0] == '.') {
+            switch (do_meta_command(input_buffer)) {
+            case META_COMMAND_SUCCESS:
+                return;
+            case META_COMMAND_UNRECOGNIZED_COMMAND:
+                printf("Unrecognized command\n");
+                return;
+            }
+        }
+    }
+
+    Statement statement;
+    switch (prepare_statement(input_buffer, &statement)) {
+    case PREPARE_SUCCESS:
+        break;
+    case PREPARE_UNRECOGNIZED_STATEMENT:
+        printf("Unrecognized keyword at start of '%s'.\n",
+            input_buffer->buffer);
+        return;
+    }
+
+    execute_statement(&statement);
+    printf("Executed.\n");
+}
+
+MetaCommandResult do_meta_command(InputBuffer *input_buffer)
+{
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
         exit(EXIT_SUCCESS);
     } else {
-        printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
+{
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if (strcmp(input_buffer->buffer, "select") == 0) {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement *statement)
+{
+    switch (statement->type) {
+    case STATEMENT_INSERT:
+        printf("This is where we would do an insert.\n");
+        break;
+    case STATEMENT_SELECT:
+        printf("This is where we would do a select.\n");
+        break;
     }
 }
